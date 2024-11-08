35df0a738208ee27cd2e099271a2b24ff88c30a4
Eduardo Romero <eduardodejesus.romero@nttdata.com>
2024-11-08 13:10:18 -0600

    firestore users table service

--- lib/services/user/user_service.dart
+++ lib/services/user/user_service.dart
@@ -1,18 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:roomies_app/models/user_model.dart';
 import 'package:roomies_app/services/firestore/firestore_service.dart';
-import 'package:roomies_app/services/secure_sotrage/secure_storage_service.dart';
 import 'package:roomies_app/utils/firestore_keys.dart';
-import 'package:roomies_app/utils/secure_storage_keys.dart';
 
 class UserService {
-  Future<UserModel?> getUser() async {
+  Future<UserModel?> getUser(String uid) async {
     try {
-      String? uid = await SecureStorageService().read(SecureStorageKeys.uid);
-      if (uid == null) {
-        Exception('No UID found in secure storage');
-        return null;
-      }
       DocumentSnapshot userDoc =
           await FirestoreService().readDocument(FirestoreKeys.users, uid);
       if (userDoc.exists) {
